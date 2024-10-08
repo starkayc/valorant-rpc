ecf1d602ab3e9fa96eaa2f218479ecc83c957167
starkayc <5252904+starkayc@users.noreply.github.com>
2024-10-08 05:52:50 -0400

    update github links again

--- src/presence/presence_utilities.py
+++ src/presence/presence_utilities.py
@@ -92,7 +92,7 @@ class Utilities:
         '''
         if presence is None:
             presence = client.fetch_presence()
-        base_api_url = "https://colinhartigan.github.io/valorant-rpc?redir={redirect}&type={req_type}"
+        base_api_url = "https://starkayc.github.io/valorant-rpc?redir={redirect}&type={req_type}"
         base_api_url = f"{base_api_url}&region={client.region}&playername={client.player_name}&playertag={client.player_tag}" # add on static values (region/playername)
         if int(presence["partySize"]) < int(presence["maxPartySize"]):
             if presence["partyAccessibility"] == "OPEN" and config["presences"]["menu"]["show_join_button_with_open_party"]:
